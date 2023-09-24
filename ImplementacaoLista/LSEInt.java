75257b99c7e4b145e93ccb16b35e3644d14ae2b7
AlefSRGS <alefsrodrigues03@gmail.com>
2023-09-23 21:06:50 -0300

    atv1 LSEnode completa sem main

---
+++ ImplementacaoLista/LSEInt.java
@@ -0,0 +1,58 @@
+package ImplementacaoLista;
+
+public class LSEInt {
+    private LSENode firstNode;
+
+    public boolean isEmpty(){
+        if(firstNode == null){
+            return true;
+        }
+        return false;
+    }
+
+    public void insertFirst(int valueNewNode){
+        LSENode newNode = new LSENode(valueNewNode);
+        if(isEmpty()){
+            firstNode = newNode;
+        }
+        newNode.setAdressNextNode(firstNode);
+        firstNode = newNode;
+        System.out.println("Inserido com sucesso!");
+    }
+
+    public void removeFirstNode(){
+        if(!isEmpty()){
+            firstNode = firstNode.getAdressNextNode();
+            System.out.println("Removido com sucesso!");
+        }
+        System.out.println("Lista vazia, nada a remover");
+    }
+
+    public void showAll(){
+        LSENode auxNode;
+        if(isEmpty()){
+            System.out.println("Lista vazia");
+            return;
+        }
+        auxNode = firstNode;
+        while(auxNode != null){
+            System.out.println(auxNode.getDataNode());
+            auxNode = auxNode.getAdressNextNode();
+        }
+    }
+
+    public int countDataInList(int dataSought){
+        if(isEmpty()){
+            return 0;
+        }
+        int count = 0;
+        LSENode auxNode = firstNode;
+        while(auxNode != null){
+            if(auxNode.getDataNode() == dataSought){
+                count++;
+            }
+            auxNode = auxNode.getAdressNextNode();
+        }
+        return count;
+    }
+}
---
+++ ImplementacaoLista/LSENode.java
@@ -0,0 +1,24 @@
+package ImplementacaoLista;
+
+public class LSENode {
+
+    private int dataNode;
+    private LSENode adressNextNode;
+
+    public LSENode(int dataNode){
+        this.dataNode = dataNode;
+    }
+    
+    public LSENode getAdressNextNode() {
+        return adressNextNode;
+    }
+    public int getDataNode() {
+        return dataNode;
+    }
+    public void setAdressNextNode(LSENode adressNextNode) {
+        this.adressNextNode = adressNextNode;
+    }
+    public void setDataNode(int dataNode) {
+        this.dataNode = dataNode;
+    }
+}
